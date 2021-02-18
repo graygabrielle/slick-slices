0e4e16f29eebbed4aad42cb9f64dee9351605973
graygabrielle <gray.atkinson.adams@gmail.com>
2021-02-18 22:38:19 +0100

    add gatsby config and first query from pizzas page

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,36 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  console.log(data);
   return (
     <>
-      <h3>Pizzas here</h3>
+      <h3>There are {data.pizzas.nodes.length} pizzas here</h3>
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
